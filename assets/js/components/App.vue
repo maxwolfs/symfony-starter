71751d32974a7d3ec149e4d95ee575191dc37290
Maximilian Wolfs <max.wolfs@homevoice.io>
2018-09-26 17:38:55 +0200

    init

---
+++ assets/js/components/App.vue
@@ -0,0 +1,11 @@
+<template>
+<div>
+  <h1>Welcome to Symfony and Vue.js</h1>
+  <p>Lorem Ipsum dolor sit</p>
+  <button class="btn btn-primary">Nice</button>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
